941ce0d50a7effd5ae3c5923ae1aec650f79d496
SKLAPTOP\skees <skeesen8@gmail.com>
2025-03-10 13:55:13 -0500

    login authentication works need to create persisting auth and update navbar routes.

---
+++ .venv/frontend/src/Homescreen.js
@@ -0,0 +1,12 @@
+import Navbar from "./navbar";
+
+const Home = () => {
+    return(
+        <div>
+            <Navbar/>    
+            <h1>Home</h1>
+        </div>
+    )   
+}
+
+export default Home;
